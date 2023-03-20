7146bb967ab7d9eb77099ef8616c559a3ea827fa
pskucherov <pskucherov@yandex.ru>
2023-03-20 17:04:59 +0300

    eslint правила

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,14 +1,15 @@
-import { Server } from 'Socket.IO'
+import { Server } from 'socket.io';
 
 const SocketHandler = (req, res) => {
     if (res.socket.server.io) {
-        console.log('Socket is already running')
+        console.log('Socket is already running'); // eslint-disable-line no-console
     } else {
-        console.log('Socket is initializing')
-        const io = new Server(res.socket.server)
-        res.socket.server.io = io
-    }
-    res.end()
-}
+        console.log('Socket is initializing'); // eslint-disable-line no-console
+        const io = new Server(res.socket.server);
 
-export default SocketHandler
+        res.socket.server.io = io;
+    }
+    res.end();
+};
+
+export default SocketHandler;
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,37 @@
-import Head from 'next/head'
-import styles from '@/styles/Home.module.css'
-import { useCallback, useEffect, useState } from 'react'
-import io from 'Socket.IO-client'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useCallback, useEffect, useState } from 'react';
+import sIo from 'socket.io-client';
 
 export default function Home() {
-  const [socket, setSocket] = useState();
+    const [socket, setSocket] = useState();
 
-  const socketInitializer = useCallback(async () => {
-    await fetch('/api/socket')
-    const socket = io()
+    const socketInitializer = useCallback(async () => {
+        await fetch('/api/socket');
+        const socket = sIo();
 
-    socket.on('connect', () => {
-      console.log('connected');
-    })
+        socket.on('connect', () => {
+            console.log('connected'); // eslint-disable-line no-console
+        });
 
-    setSocket(socket);
-  }, [setSocket]);
+        setSocket(socket);
+    }, [setSocket]);
 
-  useEffect(() => {
-    socketInitializer()
-  }, [socketInitializer]);
+    useEffect(() => {
+        socketInitializer();
+    }, [socketInitializer]);
 
-  return (
-    <>
-      <Head>
-        <title>TCS Stat</title>
-        <meta name="description" content="https://t.me/opexflow" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-                
-      </main>
-    </>
-  )
+    return (
+        <>
+            <Head>
+                <title>TCS Stat</title>
+                <meta name="description" content="https://t.me/opexflow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+
+            </main>
+        </>
+    );
 }
